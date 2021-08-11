f65b0a175e2506f9312035052d7d264df2e9c7ab
Dylan Howard <dylanhoward0424@outlook.com>
2021-08-11 16:48:01 -0500

    removed unnecessary text from app.js since DinnerSupplies Component works now

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -104,16 +104,6 @@ function App() {
       </form>
       <GuestList guestList={guestList} />
       <DinnerSupplies guestList={guestList} />
-      {/* <h2>Dinner Supplies</h2>
-      <div>
-        Spoons: {guestList.length * 2}
-      </div>
-      <div>
-        Forks: {guestList.length * 2}
-      </div>
-      <div>
-        Knives: {guestList.length * 2}
-      </div> */}
       <Footer />
     </div>
   );
